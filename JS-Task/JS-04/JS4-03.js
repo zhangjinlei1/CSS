c4d931756a256781afbadbacbfae4bc5e3035b0a
Raninbow <330252125@qq.com>
2016-09-05 15:27:12 +0800

    1

--- JS-Task/JS-04/JS4-03.js
+++ JS-Task/JS-04/JS4-03.js
@@ -40,24 +40,48 @@ console.log('杀手数:'+SS);
 console.log('平民数:'+PM);
 
 
+switch (SS){
+    case PM:
+        $('.page-footer-button').text('查看结果');
+        alert('杀手胜利');
+        break;
+    case  0:
+        $('.page-footer-button').text('查看结果');
+        alert('平民胜利');
+        break;
+}
+
 $('.page-footer-button').click(function(){
-    //location.href ="Js4-02.html";
-    if (SS===0){
-        location.href ="JS4-04.html";
-    }
-    else if(SS==PM){
-        location.href ="JS4-04.html";
-    }
-    else {
-        location.href ="Js4-02.html";
-    }
+   switch (SS){
+       case PM:
+           location.href='JS4-04.html';
+           break;
+       case 0:
+           location.href='JS4-04.html';
+           break;
+       default:
+           location.href='Js4-02.html;'
+   }
 });
 
-if(SS===0){
-    $('.page-footer-button').text('查看结果');
-    alert('游戏结束，平民胜利');
-}
-if (SS==PM){
-    $('.page-footer-button').text('查看结果');
-    alert('游戏结束，杀手胜利');
-}
+//$('.page-footer-button').click(function(){
+//    //location.href ="Js4-02.html";
+//    if (SS===0){
+//        location.href ="JS4-04.html";
+//    }
+//    else if(SS==PM){
+//        location.href ="JS4-04.html";
+//    }
+//    else {
+//        location.href ="Js4-02.html";
+//    }
+//});
+//
+//if(SS===0){
+//    $('.page-footer-button').text('查看结果');
+//    alert('游戏结束，平民胜利');
+//}
+//if (SS==PM){
+//    $('.page-footer-button').text('查看结果');
+//    alert('游戏结束，杀手胜利');
+//}
