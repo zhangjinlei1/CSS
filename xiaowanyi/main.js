1503312c4275d2c4e5373478defed96eb0ab1a71
Raninbow <330252125@qq.com>
2016-09-30 17:28:10 +0800

    1

--- xiaowanyi/main.js
+++ xiaowanyi/main.js
@@ -3,10 +3,28 @@
  */
 var app = angular.module("myApp",[]);
     app.controller("cartController",function($scope){
-        $scope.cart={
+        $scope.cart=[{
             id:1000,
-            name:"in"
+            name:"iphone5s",
+            quantity:"3",
+            price:"4300"
+        },{
+            id:3300,
+            name:"iphone5",
+            quantity:"30",
+            price:"3300"
+        },{
+            id:232,
+            name:"imac",
+            quantity:"4",
+            price:"3000"
+        },{
+            id:1000,
+            name:"ipad",
+            quantity:"3",
+            price:"4300"
         }
+        ]
     });
 
 
