c49e7935e71e89b48d50c24e625313b3628a0be0
Raninbow <330252125@qq.com>
2016-09-30 16:27:20 +0800

    1

--- xiaowanyi/main.js
+++ xiaowanyi/main.js
@@ -1,9 +1,13 @@
 /**
  * Created by Raninbow on 2016/9/18.
  */
-
-
-
+var app = angular.module("myApp",[]);
+    app.controller("cartController",function($scope){
+        $scope.cart={
+            id:1000,
+            name:"in"
+        }
+    });
 
 
 
