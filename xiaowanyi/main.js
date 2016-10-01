4e6a5d40528bef1135ba094094c57cc9ef699689
Raninbow <330252125@qq.com>
2016-10-01 14:20:18 +0800

    1

--- xiaowanyi/main.js
+++ xiaowanyi/main.js
@@ -6,25 +6,56 @@ var app = angular.module("myApp",[]);
         $scope.cart=[{
             id:1000,
             name:"iphone5s",
-            quantity:"3",
+            quantity:3,
             price:"4300"
         },{
             id:3300,
             name:"iphone5",
-            quantity:"30",
+            quantity:30,
             price:"3300"
         },{
             id:232,
             name:"imac",
-            quantity:"4",
+            quantity:4,
             price:"3000"
         },{
             id:1000,
             name:"ipad",
-            quantity:"3",
+            quantity:3,
             price:"4300"
         }
-        ]
+        ];
+    //    计算总价
+        $scope.totalPrice=function(){
+            var total=0;
+            angular.forEach($scope.cart,function(x){
+                total+= x.quantity* x.price
+            });
+            return total
+        };
+        //计算总数量
+        $scope.totalNum=function(){
+            var total=0;
+            angular.forEach($scope.cart,function(x){
+                total+= x.quantity
+            });
+            return total
+        };
+        //删除当前数据
+        $scope.remove=function(id){
+            var index=-1;
+            angular.forEach($scope.cart,function(x,key){
+                if(x.id===id){
+                    index=key;
+                }
+            });
+            if(index!==-1){
+                $scope.cart.splice(index,1);
+            }
+        };
+        $scope.DeleteOn=function(){
+            $scope.cart={};
+        }
     });
 
 
